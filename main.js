cf8a9be3574198264cee2cc79d2876d2506b35c7
Matthew Sheahan <s534141@nwmissouri.edu>
2019-09-30 12:47:45 -0500

    Added some responsiveness

--- main.js
+++ main.js
@@ -13,15 +13,12 @@ const validate = async (event) => {
 
 const updateWithCTemp = async (event) => {
   document.querySelector('#tempC').innerHTML = ''
-  const t1 = parseDouble(document.querySelector('#tempK').value)
+  const tempK = parseInt(document.querySelector('#tempK').value)
   const tempConvert = 273.15
-  const tempC = (t1-tempConvert)
+  const tempC = (tempK-tempConvert)
   const ans = `The temperature in Celcius is ${tempC.toFixed(2)} degrees`
   document.querySelector('#tempC').innerHTML = ans
 }
-
-// delegate to dynamic elements (e.g. when testing)
-// focusout is like blur, but it bubbles up
 
 document.addEventListener('click', event => {
   if (event.target && event.target.id === 'clicker') { updateWithCTemp(event) }
